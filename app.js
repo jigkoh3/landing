5aac89a176ca07777ee843b32750f56a13b65284
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2020-01-07 09:28:28 +0700

    update route

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/landing/index.html'));
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("/product_case.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/landing/product_case.html"));
 });
 
-const port = process.env.PORT || '3001';
-app.set('port', port);
+app.get("/gallery_3.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/landing/gallery_3.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/landing/index.html"));
+});
+
+const port = process.env.PORT || "3001";
+app.set("port", port);
 const server = http.createServer(app);
-server.listen(port, () => console.log('Running'));
+server.listen(port, () => console.log("Running"));
